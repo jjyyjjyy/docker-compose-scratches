ad993e752b89305aad831b1d1dfa3a2e6b59ec31
jy <977996067@qq.com>
2018-09-30 15:38:11 +0800

    :sparkless: Upgrade portainer

--- rabbitmq/delay-plugin.sh
+++ rabbitmq/delay-plugin.sh
@@ -1,5 +1,4 @@
-wget "https://bintray.com/rabbitmq/community-plugins/download_file?file_path=3.7.x%2Frabbitmq_delayed_message_exchange%2Frabbitmq_delayed_message_exchange-20171201-3.7.x.zip" -O rabbit-delay-plugin.zip
-unzip -q rabbit-delay-plugin.zip
+wget https://soft-1252259164.cos.ap-shanghai.myqcloud.com/rabbitmq_delayed_message_exchange-20171201-3.7.x.ez
 docker cp rabbitmq_delayed_message_exchange-20171201-3.7.x.ez rabbitmq:/plugins/rabbitmq_delayed_message_exchange-0.0.1.x.ez
 docker exec rabbitmq rabbitmq-plugins enable rabbitmq_delayed_message_exchange
 rm -rf rabbit-delay-plugin.zip rabbitmq_delayed_message_exchange-20171201-3.7.x.ez
